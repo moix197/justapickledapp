e45ca0b1c2682d43fee6bf131adf87e776984bb2
Moises Montilla <cl_moises@hotmail.com>
2024-03-21 18:10:13 -0300

    added the wrong db conn so now fixing it

--- src/utils/back/db/db.js
+++ src/utils/back/db/db.js
@@ -4,8 +4,8 @@ import { MongoClient } from "mongodb";
 
 //const uri = "mongodb://Admin:%4019721562aA@109.123.250.102:27017/"; // MongoDB URI
 
-//const uri = "mongodb://Admin:%4019721562aA@localhost:27017"; // MongoDB URI
-const uri = "mongodb://localhost:27017";
+const uri = "mongodb://Admin:%4019721562aA@localhost:27017"; // MongoDB URI
+//const uri = "mongodb://localhost:27017";
 
 const dbName = "pickle_db"; // Name of your MongoDB database
 
