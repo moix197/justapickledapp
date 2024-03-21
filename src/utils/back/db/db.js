615c327c4ac730fc0fe16a0c0b7e14da65d7b4ee
Moises Montilla <cl_moises@hotmail.com>
2024-03-21 00:54:43 -0300

    fixed small issue with db conn

--- src/utils/back/db/db.js
+++ src/utils/back/db/db.js
@@ -4,7 +4,7 @@ import { MongoClient } from "mongodb";
 
 //const uri = "mongodb://Admin:%4019721562aA@109.123.250.102:27017/"; // MongoDB URI
 
-const uri = "mongodb:Admin:@19721562aA@localhost:27017"; // MongoDB URI
+const uri = "mongodb://Admin:%4019721562aA@localhost:27017"; // MongoDB URI
 
 const dbName = "pickle_db"; // Name of your MongoDB database
 
